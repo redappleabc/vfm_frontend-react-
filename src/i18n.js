35d24ece328813bcd922c383cf07f3497e1c85bd
codegenius2 <lucifer231220@gmail.com>
2024-07-12 13:35:46 +0900

    Feat: init

---
+++ src/i18n.js
@@ -0,0 +1,61 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// Import translation files
+import translationEN from './locales/en/translation.json';
+import translationJP from './locales/jap/translation.json';
+import translationZH from './locales/zh/translation.json';
+import translationKO from './locales/ko/translation.json';
+
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  jap: {
+    translation: translationJP,
+  },
+  zh: {
+    translation: translationZH,
+  },
+  ko: {
+    translation: translationKO,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en', // default language
+    fallbackLng: 'en', // fallback language
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+export default i18n;
+// import i18n from 'i18next';
+// import { initReactI18next } from 'react-i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+// import HttpApi from 'i18next-http-backend';
+
+// i18n
+//   .use(initReactI18next)
+//   .use(LanguageDetector)
+//   .use(HttpApi)
+//   .init({
+//     fallbackLng: 'en',
+//     detection: {
+//       order: ['cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+//       caches: ['cookie'],
+//     },
+//     backend: {
+//       loadPath: '/locales/{{lng}}/translation.json',
+//     },
+//     react: {
+//       useSuspense: false,
+//     },
+//   });
+
+// export default i18n;
